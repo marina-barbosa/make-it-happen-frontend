589134bf3a614fbc3a9a19cd0d8ffe301c103b5b
gessicafontes <gessicapimentelfontes@gmail.com>
2024-11-17 20:43:37 -0300

    feat: login/registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormLoginComponent } from './components/form-login/form-login.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormRegisterComponent } from './components/form-register/form-register.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FormLoginComponent,
+    LoginComponent,
+    FormRegisterComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
